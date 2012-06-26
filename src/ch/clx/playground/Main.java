dd2288fa7aab7d150b605e304964f2ea53231bc8
Adrian Zenobi <adrian.zenobi@crealogix.com>
2012-06-26 14:03:09 +0200

    First Version Commit

---
+++ src/ch/clx/playground/Main.java
@@ -0,0 +1,12 @@
+package ch.clx.playground;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: azen
+ * Date: 26.06.12
+ * Time: 13:42
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+
+}
